348257b2ea926d0e0fba99fa54c4795b31f589f7
gryphonrobotics <gryphonrobotics@csus.org>
2016-02-13 15:21:08 -0800

    Added basic scripting autonomous

---
+++ src/Configs/CONFIGS.h
@@ -0,0 +1,22 @@
+/*
+ * CONFIGS.h
+ *
+ *  Created on: Feb 13, 2016
+ *      Author: user
+ */
+
+#ifndef SRC_CONFIGS_CONFIGS_H_
+#define SRC_CONFIGS_CONFIGS_H_
+
+namespace CONFIGS {
+	//in m/s
+	double MAX_SPEED = 2.7432;
+
+	//so the robot doesn't go max speed
+	double MOTOR_SCALE = .85;
+
+	//robot width in m
+	double ROBOT_WIDTH = .86;
+}
+
+#endif /* SRC_CONFIGS_CONFIGS_H_ */
