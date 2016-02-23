12862d9602a84b58837a54f933373eeea808c857
jlange16 <jlange@crystal.csus.org>
2016-02-22 18:06:01 -0800

    Update AutoDriveController.cpp
    No longer uses while loops, but also doesn't iterate. Still needs work...

--- src/DriveControllers/AutoDriveController.cpp
+++ src/DriveControllers/AutoDriveController.cpp
@@ -28,17 +28,30 @@ public:
 
 		//assuming time_t is seconds
 		auto end_time = std::time(nullptr) + (std::abs(metres) / CONFIGS::MAX_SPEED);
-
-		if(metres > 0)
+		
+		//no longer uses a while loop; uses the bool should_move instead
+		if(metres > 0) //to go forwards
 		{
-			while(std::time(nullptr) < end_time)
+			static bool should_move = true;
+			
+			if(std::time(nullptr) >= end_time)
+			{
+				should_move = false;
+			}
+			if(should_move)
 			{
 				_adc->_db->setAll(CONFIGS::MOTOR_SCALE);
 			}
 		}
-		else if(metres < 0)
+		else if(metres < 0) //to go backwards
 		{
-			while(std::time(nullptr) < end_time)
+			static bool should_move = true;
+			
+			if(std::time(nullptr) >= end_time)
+			{
+				should_move = false;
+			}
+			if(should_move)
 			{
 				_adc->_db->setAll(-CONFIGS::MOTOR_SCALE);
 			}
@@ -50,22 +63,35 @@ public:
 	{
 		//assuming degrees!
 		constexpr int deg_in_circle = 360;
-		double distance = (CONFIGS::ROBOT_WIDTH * M_PI * degrees) / deg_in_circle;
+		double distance = (CONFIGS::ROBOT_WIDTH * M_PI) * (degrees / deg_in_circle); //arc length of the turn
 
 		constexpr char double_motors = 2;
-		auto end_time = std::time(nullptr) + (std::abs(distance) / CONFIGS::MAX_SPEED * double_motors);
+		auto end_time = std::time(nullptr) + (std::abs(distance) / (CONFIGS::MAX_SPEED * double_motors)); //distance = speed * time. I still think this might turn half the intended angle.
 
-		if(degrees > 0)
+		//no longer uses a while loop; uses bool should_turn instead
+		if(degrees > 0) //clockwise
 		{
-			while(std::time(nullptr) < end_time)
+			static bool should_turn = true;
+			
+			if(std::time(nullptr) >= end_time)
+			{
+				should_turn = false;
+			}
+			if(should_turn)
 			{
 				_adc->_db->setSide(CONFIGS::MOTOR_SCALE, Side::Right);
 				_adc->_db->setSide(-CONFIGS::MOTOR_SCALE, Side::Left);
 			}
 		}
-		else if(degrees < 0)
+		else if(degrees < 0) //counterclockwise
 		{
-			while(std::time(nullptr) < end_time)
+			static bool should_turn = true;
+			
+			if(std::time(nullptr) >= end_time)
+			{
+				should_turn = false;
+			}
+			if(should_turn)
 			{
 				_adc->_db->setSide(-CONFIGS::MOTOR_SCALE, Side::Right);
 				_adc->_db->setSide(CONFIGS::MOTOR_SCALE, Side::Left);
